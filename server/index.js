75b17923d5ff6ca40f47f8ae9a3f13e4d50e0b0a
Abhishek-patole <abhishekpatoles13@gmail.com>
2025-11-02 20:03:57 +0530

    error soloving

--- server/index.js
+++ server/index.js
@@ -1,4 +1,7 @@
 require('dotenv').config();
+console.log("--- !!! STARTING APP: CHECKING ENV VARS !!! ---");
+console.log("My MONGO_URI is:", process.env.MONGO_URI);
+console.log("My JWT_SECRET is:", process.env.JWT_SECRET);
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
